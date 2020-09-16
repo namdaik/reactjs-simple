1866c1199e7fded38035f92e606e06a1342ab291
Your Name <coibg1368@gmail.com>
2020-09-16 10:37:42 +0000

    update

--- src/module/fake-content/FakeContentUrl.js
+++ src/module/fake-content/FakeContentUrl.js
@@ -1,7 +1,7 @@
 import ParseHtml from '../parse-html/ParseHtml';
 
 export default class FakeContentUrl {
-    headingElement = [];
+    headingFake = [];
     pElement = [];
     linkInfo = [];
     
@@ -10,11 +10,32 @@ export default class FakeContentUrl {
 	}
  	parseAll() {
  		var partHtml1 = new ParseHtml();
-        
         partHtml1.parseAll().then(result => {
-            var headingElement, pElement, linkInfo;
-            [headingElement, pElement, linkInfo] = result;
-            console.log(headingElement, pElement, linkInfo);
+            var headings, p, linkInfo;
+            [headings, p, linkInfo] = result;
+            this.fakeHeading(headings);
         });
  	}
+    fakeHeading(headings) {
+        let headingFake = {} 
+        for (const htag in headings) {
+            var arrayH = [];
+            headings[htag].forEach(itemdata => {
+                arrayH.push(itemdata);
+            });
+            headingFake[htag] = arrayH;
+        }
+        this.headingFake = headingFake;
+    }
+    fakeP(headings) {
+        let headingFake = {} 
+        for (const htag in headings) {
+            var arrayH = [];
+            headings[htag].forEach(itemdata => {
+                arrayH.push(itemdata);
+            });
+            headingFake[htag] = arrayH;
+        }
+        this.headingFake = headingFake;
+    }
 }
